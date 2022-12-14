c7306503ff6d13117a5d7d67600e8b19b05c0c86
German Shein <germanshein1995@gmail.com>
2022-12-14 16:01:28 +0400

    Categories: Preparing carousel integration

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,6 +1,6 @@
 export const Configuration =
 {
-	API_URL: 'http://localhost:3030/api',
-	COS_URL: 'http://localhost:3030',
+	API_URL: 'http://192.168.137.1:3030/api',
+	COS_URL: 'http://192.168.137.1:3030',
 	Language: 'en-US'
 };
