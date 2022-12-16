ec71320aad50470c309c17776cb0fa0020606c6d
German Shein <germanshein1995@gmail.com>
2022-12-16 23:54:15 +0400

    Carousel: Making it look presentable

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -7,38 +7,33 @@ const Categories = ({Colors, Sections, Section_References}) =>
 {
 	const Navigation_Bar_Reference = useRef ();
 
-	const settings = {
+	const settings = 
+	{
 		dots: true,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-        ]
-	  };
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		responsive: 
+		[
+			{
+				breakpoint: 1024,
+				settings: 
+				{
+					slidesToShow: 3,
+					slidesToScroll: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: 
+				{
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}
+		]
+	};
 
 	const Scroll_to_the_Section = Index =>
 	{
@@ -48,7 +43,7 @@ const Categories = ({Colors, Sections, Section_References}) =>
 		}
 	}
 
-	/*return <nav className='Categories' key='Menu_Categories_Key' ref={Navigation_Bar_Reference} style={{backgroundColor: Colors.Background, color: Colors.Text, top: Navigation_Bar_Reference.current ? (Navigation_Bar_Reference.current.children [0].children [1].clientHeight - Navigation_Bar_Reference.current.clientHeight).toString () + 'px' : '0px'}}>
+	return <nav className='Categories' key='Menu_Categories_Key' ref={Navigation_Bar_Reference} style={{backgroundColor: Colors.Background, color: Colors.Text, top: Navigation_Bar_Reference.current ? (Navigation_Bar_Reference.current.children [0].children [1].clientHeight - Navigation_Bar_Reference.current.clientHeight).toString () + 'px' : '0px'}}>
 			<Slider {...settings}>
 			{Sections.map ((Category, Index) => 
 				<div className="Category" key={Category.Name.replace (' ', '_') + '_Category_Key'} onClick={() => Scroll_to_the_Section (Index)}>
@@ -56,14 +51,6 @@ const Categories = ({Colors, Sections, Section_References}) =>
 					<div className='Category_Name' key={Category.Name.replace (' ', '_') + '_Category_Name_Key'}>{Category.Name}</div>
 				</div>)}
 			</Slider>
-	</nav>;*/
-
-    return <nav className='Categories' key='Menu_Categories_Key' ref={Navigation_Bar_Reference} style={{backgroundColor: Colors.Background, color: Colors.Text, top: Navigation_Bar_Reference.current ? (Navigation_Bar_Reference.current.children [0].children [1].clientHeight - Navigation_Bar_Reference.current.clientHeight).toString () + 'px' : '0px'}}>
-			{Sections.map ((Category, Index) => 
-				<div className="Category" key={Category.Name.replace (' ', '_') + '_Category_Key'} onClick={() => Scroll_to_the_Section (Index)}>
-					<img alt={Category.Name + ' Image'} className='Category_Image' key={Category.Name.replace (' ', '_') + '_Category_Image_Key'} src={Category.Banner_Image}></img>
-					<div className='Category_Name' key={Category.Name.replace (' ', '_') + '_Category_Name_Key'}>{Category.Name}</div>
-				</div>)}
 	</nav>;
 }
 
